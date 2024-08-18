d771e8b56b7ac5ed1f2eb31466dc95b5944c4e2d
KevinHung <kevinaugust145@gmail.com>
2024-08-18 12:21:54 +0800

    Initial Commit

---
+++ MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by 洪宗燦 on 2024/8/18.
+//
+
+import Testing
+@testable import MovieApp
+
+struct MovieAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
